4347e4ccb8b06adfdcbb6db924a1b3cc2a8fed50
E171932 <E171932@e9c31dfe-1851-1a49-92b9-e739eadd3f68>
2013-10-07 07:24:51 +0000

    inkey by curses
    git-svn-id: file:///cygdrive/c/Users/E171932/svnroot/trunk/z80emu@217 e9c31dfe-1851-1a49-92b9-e739eadd3f68

--- inkey.c
+++ inkey.c
@@ -1,10 +1,15 @@
-#include	<sgtty.h>
-
-int	main()
+#include	<ncurses/curses.h>
+int main(void)
 {
-	int	ret, n;
+	int	c;
 
-	
-	ret = ioctl(0, FIONREAD, &n);
-	printf("ret=%d, n=%d\n", ret, n);
+	initscr();
+	cbreak();
+	noecho();
+	timeout(0);
+	while((c=getch()) == -1) {
+		addstr("."); refresh();
+		usleep(10000); // 10ms
+	}
+	endwin();  
 }
